a5bc5489d33e9b37edd6cb093c4a03d486d819db
Artem <artem.aisin@yandex.ru>
2024-03-19 01:20:22 +0300

    feat: sidebar open

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,40 @@
+import { useState } from 'react';
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Select } from '../select';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
+import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
+			<aside
+				className={clsx({
+					[styles.container]: true,
+					[styles.container_open]: isOpen,
+				})}>
 				<form className={styles.form}>
+					<h3>задайте параметры</h3>
+					<Select selected={null} options={[]}></Select>
+					<RadioGroup
+						name={'Размер шрифта'}
+						options={[]}
+						selected={{
+							title: 'ewr',
+							value: 'wer',
+							className: 'wer',
+							optionClassName: undefined,
+						}}
+						title={''}></RadioGroup>
+					<Select selected={null} options={[]}></Select>
+					<Select selected={null} options={[]}></Select>
+					<Separator></Separator>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />

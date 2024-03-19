9c352084b4b1e02d729c0d38cd1d0057d94bb334
Artem <artem.aisin@yandex.ru>
2024-03-19 20:12:35 +0300

    feat: styles change

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect, FC } from 'react';
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 import { Select } from '../select';
 import { RadioGroup } from '../radio-group';
 import { Separator } from '../separator';
 import {
-	fontFamilyClasses,
 	OptionType,
 	fontFamilyOptions,
 	fontColors,
@@ -25,75 +24,114 @@ type ArticleParamsFormProps = {
 };
 
 export const ArticleParamsForm = ({
-	articleState,
 	setArticleState,
 }: ArticleParamsFormProps) => {
+	const [state, setState] = useState(defaultArticleState);
 	const [isOpen, setIsOpen] = useState(false);
+	const asideRef = useRef<HTMLDivElement>(null);
+	const arrowRef = useRef<HTMLDivElement>();
 
-	const onChangeFontFamily = (selected: OptionType) => {
-		setArticleState({
-			...articleState,
+	useEffect(() => {
+		const isClickedOutside = (evt: any) => {
+			if (asideRef.current && !asideRef.current.contains(evt.target)) {
+				setIsOpen(false);
+			}
+		};
+		document.addEventListener('mousedown', isClickedOutside);
+		return () => {
+			document.removeEventListener('mousedown', isClickedOutside);
+		};
+	}, []);
+
+	const handleOnChangeFontFamily = (selected: OptionType) => {
+		setState({
+			...state,
 			fontFamilyOption: selected,
 		});
 	};
 
-	const onChangeFontSize = (selected: OptionType) => {
-		setArticleState({
-			...articleState,
+	const handleOnChangeFontSize = (selected: OptionType) => {
+		setState({
+			...state,
 			fontSizeOption: selected,
 		});
 	};
 
-	const onChangeFontColor = (selected: OptionType) => {
-		setArticleState({
-			...articleState,
+	const handleOnChangeFontColor = (selected: OptionType) => {
+		setState({
+			...state,
 			fontColor: selected,
 		});
 	};
 
-	const onChangeBackgroundColor = (selected: OptionType) => {
-		setArticleState({
-			...articleState,
+	const handleOnChangeBackgroundColor = (selected: OptionType) => {
+		setState({
+			...state,
 			backgroundColor: selected,
 		});
+	};
+
+	const handleOnChangeContentWidth = (selected: OptionType) => {
+		setState({
+			...state,
+			contentWidth: selected,
+		});
+	};
+
+	const handleResetArticleState = (evt: React.FormEvent<HTMLFormElement>) => {
+		// console.log(evt);
+		evt.preventDefault();
+		setArticleState({ ...defaultArticleState });
+		setState({ ...defaultArticleState });
+	};
+
+	const handleApplyArticleState = (evt: React.FormEvent<HTMLFormElement>) => {
+		// console.log(evt);
+		evt.preventDefault();
+		setArticleState({ ...state });
 	};
 
 	return (
 		<>
 			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
 			<aside
+				ref={asideRef}
 				className={clsx({
 					[styles.container]: true,
 					[styles.container_open]: isOpen,
 				})}>
-				<form className={styles.form}>
+				<form
+					className={styles.form}
+					onSubmit={handleApplyArticleState}
+					onReset={handleResetArticleState}>
 					<h3 className={styles.formTitle}>задайте параметры</h3>
 					<Select
 						title={'шрифт'}
-						onChange={onChangeFontFamily}
-						selected={articleState.fontFamilyOption}
+						onChange={handleOnChangeFontFamily}
+						selected={state.fontFamilyOption}
 						options={fontFamilyOptions}></Select>
 					<RadioGroup
 						name={'размер шрифта'}
 						options={fontSizeOptions}
-						onChange={onChangeFontSize}
-						selected={articleState.fontSizeOption}
+						onChange={handleOnChangeFontSize}
+						selected={state.fontSizeOption}
 						title={'размер шрифта'}></RadioGroup>
 					<Select
 						title={'цвет шрифта'}
-						onChange={onChangeFontColor}
-						selected={articleState.fontColor}
+						onChange={handleOnChangeFontColor}
+						selected={state.fontColor}
 						options={fontColors}></Select>
 					<Separator></Separator>
 					<Select
 						title={'цвет фона'}
-						onChange={onChangeBackgroundColor}
-						selected={articleState.backgroundColor}
+						onChange={handleOnChangeBackgroundColor}
+						selected={state.backgroundColor}
 						options={backgroundColors}></Select>
 					<Select
 						title={'ширина контента'}
-						selected={null}
-						options={[]}></Select>
+						onChange={handleOnChangeContentWidth}
+						selected={state.contentWidth}
+						options={contentWidthArr}></Select>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -3,15 +3,12 @@ import clsx from 'clsx';
 // Сообщаем вебпаку, что этот файл использует это изображение.
 import plane from 'src/images/plane.png';
 import { Text } from 'components/text';
+
 import styles from './Article.module.scss';
 
-type ArticleProps = {
-	onClick: () => void;
-};
-
-export const Article = ({ onClick }: ArticleProps) => {
+export const Article = () => {
 	return (
-		<article onClick={onClick} className={clsx(styles.article)}>
+		<article className={clsx(styles.article)}>
 			<Text as='h1' size={45} weight={800} uppercase dynamicLite>
 				Портрет Западной Швейцарии
 			</Text>

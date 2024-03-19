2a97531827561d20d5466bce2612b86219130f54
Artem <artem.aisin@yandex.ru>
2024-03-19 17:42:08 +0300

    feat: state and widget

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -4,12 +4,59 @@ import { Button } from 'components/button';
 import { Select } from '../select';
 import { RadioGroup } from '../radio-group';
 import { Separator } from '../separator';
+import {
+	fontFamilyClasses,
+	OptionType,
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	defaultArticleState,
+	ArticleStateType,
+} from 'src/constants/articleProps';
 import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	articleState: ArticleStateType;
+	setArticleState: (items: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	articleState,
+	setArticleState,
+}: ArticleParamsFormProps) => {
 	const [isOpen, setIsOpen] = useState(false);
+
+	const onChangeFontFamily = (selected: OptionType) => {
+		setArticleState({
+			...articleState,
+			fontFamilyOption: selected,
+		});
+	};
+
+	const onChangeFontSize = (selected: OptionType) => {
+		setArticleState({
+			...articleState,
+			fontSizeOption: selected,
+		});
+	};
+
+	const onChangeFontColor = (selected: OptionType) => {
+		setArticleState({
+			...articleState,
+			fontColor: selected,
+		});
+	};
+
+	const onChangeBackgroundColor = (selected: OptionType) => {
+		setArticleState({
+			...articleState,
+			backgroundColor: selected,
+		});
+	};
 
 	return (
 		<>
@@ -20,21 +67,33 @@ export const ArticleParamsForm = () => {
 					[styles.container_open]: isOpen,
 				})}>
 				<form className={styles.form}>
-					<h3>задайте параметры</h3>
-					<Select selected={null} options={[]}></Select>
+					<h3 className={styles.formTitle}>задайте параметры</h3>
+					<Select
+						title={'шрифт'}
+						onChange={onChangeFontFamily}
+						selected={articleState.fontFamilyOption}
+						options={fontFamilyOptions}></Select>
 					<RadioGroup
-						name={'Размер шрифта'}
-						options={[]}
-						selected={{
-							title: 'ewr',
-							value: 'wer',
-							className: 'wer',
-							optionClassName: undefined,
-						}}
-						title={''}></RadioGroup>
-					<Select selected={null} options={[]}></Select>
-					<Select selected={null} options={[]}></Select>
+						name={'размер шрифта'}
+						options={fontSizeOptions}
+						onChange={onChangeFontSize}
+						selected={articleState.fontSizeOption}
+						title={'размер шрифта'}></RadioGroup>
+					<Select
+						title={'цвет шрифта'}
+						onChange={onChangeFontColor}
+						selected={articleState.fontColor}
+						options={fontColors}></Select>
 					<Separator></Separator>
+					<Select
+						title={'цвет фона'}
+						onChange={onChangeBackgroundColor}
+						selected={articleState.backgroundColor}
+						options={backgroundColors}></Select>
+					<Select
+						title={'ширина контента'}
+						selected={null}
+						options={[]}></Select>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
@@ -13,6 +13,8 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [articleState, setArticleState] = useState(defaultArticleState);
+
 	return (
 		<div
 			className={clsx(styles.main)}
@@ -25,8 +27,15 @@ const App = () => {
 					'--bg-color': defaultArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
-			<Article />
+			<ArticleParamsForm
+				articleState={articleState}
+				setArticleState={setArticleState}
+			/>
+			<Article
+				onClick={() => {
+					console.log();
+				}}
+			/>
 		</div>
 	);
 };
